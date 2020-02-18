7e4b32743c1e07622f23c619aea5c830720974aa
Rodrigo <roi31@hotmail.com>
2020-02-18 14:40:12 +0100

    Validacion de la Search bar

--- js.js
+++ js.js
@@ -1,17 +1,41 @@
+
+// Selecciono el id="search" del imput
 const SEARCH = document.querySelector("#search");
 
-SEARCH.onkeyup = function(){
 
-    if (SEARCH.length < 3) {
-    
-        console.log("You need to enter more than 3 letters");
-        
+// Cuando se suelte la primera tecla se ejecuta la funcion
+SEARCH.onkeyup =  letterCount; 
+// Cuando se hace click fuera del imput se ejecuta la funcion
+SEARCH.onblur = cleanSearchMsg;
+
+function letterCount(){
+    // Si el length del imput menor que 3, se añade la clase "is-invalid"(clase de Bootstrap)
+    if (SEARCH.value.length < 3 ) {
+        SEARCH.classList.add("is-invalid");
+
+    // Si no se cumple el anterior, se elimina la clase "is-invalid"    
     }else{
-
-        console.log("no pasa nada");
-        
+        SEARCH.classList.remove("is-invalid");
+       
     }
+}
 
+function cleanSearchMsg(){
+
+    SEARCH.classList.remove("is-invalid");
 
 }
+
+
+// LOG IN
+
+
+
+
+
+// REGISTER FORM
+
+
+
+
 
