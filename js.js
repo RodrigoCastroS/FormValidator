83830120c52e2ca3e5532afd5bbd587d0f1b061c
Rodrigo <roi31@hotmail.com>
2020-02-19 13:55:41 +0100

    Validación de el Login

--- js.js
+++ js.js
@@ -1,6 +1,14 @@
 
 // Selecciono el id="search" del imput
 const SEARCH = document.querySelector("#search");
+const EMAIL = document.querySelector("#emailLog");
+const PASSWORD = document.querySelector("#pswLog");
+
+const EMAILREG = document.querySelector("#emailreg");
+const PSWREG1 = document.querySelector("#pswReg1");
+const PSWREG2 = document.querySelector("#pswReg2");
+
+let patt = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 
 // Cuando se suelte la primera tecla se ejecuta la funcion
@@ -29,11 +37,37 @@ function cleanSearchMsg(){
 
 // LOG IN
 
+EMAIL.onkeyup = emailValidator;
+PASSWORD.onkeyup = pswCount;
 
 
+function emailValidator(){
+    
+    if (patt.test(EMAIL.value)) {
+        EMAIL.classList.remove("is-invalid");
+        
+    }else{
+        EMAIL.classList.add("is-invalid");
+    }
+    
+}
 
+function pswCount(e){
+    
+    
+    if (PASSWORD.value.length < 5 ) {
+        PASSWORD.classList.add("is-invalid");
+        
+        // Si no se cumple el anterior, se elimina la clase "is-invalid"    
+    }else{
+        PASSWORD.classList.remove("is-invalid");
+        
+    }
+}
 
 // REGISTER FORM
+
+
 
 
 
