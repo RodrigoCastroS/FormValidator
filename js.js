8d5d2a70cdb0ab320f46de49dc7392a80c853e55
Rodrigo <roi31@hotmail.com>
2020-02-21 14:12:40 +0100

    Register Validado y completado

--- js.js
+++ js.js
@@ -4,9 +4,10 @@ const SEARCH = document.querySelector("#search");
 const EMAIL = document.querySelector("#emailLog");
 const PASSWORD = document.querySelector("#pswLog");
 
-const EMAILREG = document.querySelector("#emailreg");
+const EMAILREG = document.querySelector("#emailReg");
 const PSWREG1 = document.querySelector("#pswReg1");
 const PSWREG2 = document.querySelector("#pswReg2");
+const SELECT = document.querySelector("select");
 
 let patt = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
@@ -29,47 +30,85 @@ function letterCount(){
 }
 
 function cleanSearchMsg(){
-
     SEARCH.classList.remove("is-invalid");
-
 }
-
-
 // LOG IN
 
-EMAIL.onkeyup = emailValidator;
-PASSWORD.onkeyup = pswCount;
-
-
-function emailValidator(){
+function checkLog(){
+   
+    emailValidator();
+    pswCount();
     
-    if (patt.test(EMAIL.value)) {
-        EMAIL.classList.remove("is-invalid");
-        
-    }else{
-        EMAIL.classList.add("is-invalid");
+    function emailValidator(){   
+        if (patt.test(EMAIL.value)) {
+            EMAIL.classList.remove("is-invalid");
+            
+        }else{
+            EMAIL.classList.add("is-invalid");
+        }  
     }
     
-}
-
-function pswCount(e){
-    
-    
-    if (PASSWORD.value.length < 5 ) {
-        PASSWORD.classList.add("is-invalid");
-        
-        // Si no se cumple el anterior, se elimina la clase "is-invalid"    
-    }else{
-        PASSWORD.classList.remove("is-invalid");
-        
+    function pswCount(){   
+        if (PASSWORD.value.length < 5 ) {
+            PASSWORD.classList.add("is-invalid");
+        }else{
+            PASSWORD.classList.remove("is-invalid");   
+        }
     }
 }
-
+    
+    
+    
 // REGISTER FORM
 
 
+function checkReg(){
 
+    emailValidatorReg();
+    pswCountReg();
+    pswValidation();
 
+    // EMAILREG.onkeyup = emailValidatorReg();
+    // PSWREG1.onkeyup =  pswCountReg();
+    // PSWREG2.onkeyup =  pswValidation();
+}   
+
+    // PASSWORD.onkeyup = pswCount;
+
+    function emailValidatorReg(){
+        
+        if (patt.test(EMAILREG.value)) {
+            EMAILREG.classList.remove("is-invalid");
+        }else{
+            EMAILREG.classList.add("is-invalid");
+        }    
+    }
+
+    function pswCountReg(){
+        let psw1 = PSWREG1.value.length;
+        
+        if(psw1 < 5 ){
+            PSWREG1.classList.add("is-invalid");
+        }else{
+            PSWREG1.classList.remove("is-invalid");
+        }
+    }
+
+    function pswValidation(){
+        if(PSWREG1.value === PSWREG2.value){
+            PSWREG2.classList.remove("is-invalid");
+        }else{
+            PSWREG2.classList.add("is-invalid");
+        }
+    }
+
+    function provinceVal(){
+        if (SELECT.value == "") {
+            SELECT.classList.add("is-invalid");
+        }else{
+            SELECT.classList.remove("is-invalid");
+        }
+    }
 
 
 
